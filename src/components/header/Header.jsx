854d2aabedfb5567f1df1efff581332922051abe
DimaVnuk <vnuchok311994@hotmail.com>
2022-02-25 22:01:17 +0300

    feat: added routing

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Header() {
   return (
-	<div className="header-cont">
-		
+	<div className="header-cont header-cont_background-black">
+		<div><h1>Movie Free</h1></div>
+		<div><label><b>Search</b> <input /></label></div>
+		<div><b>Sort</b></div>
 	</div>
   )
 }
